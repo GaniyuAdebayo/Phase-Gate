aaa8d77630ca3677e0b856b8869b91c4cea4f861
Gannix <xgannix@gmail.com>
2026-03-18 16:58:02 +0100

    second task in the afternoon

--- Python/Gate Three/day_three_task.py
+++ Python/Gate Three/day_three_task.py
@@ -12,3 +12,30 @@ def task_one (numbers, list_size):
 			new_number.append(-1)
 
 	return new_number
+
+def task_three(numbers):
+
+	numberIndex = [0,0]
+	largest = numbers[0][0]
+
+	for rowIndex in range(len(numbers)):
+		for columnIndex in range(len(numbers[rowIndex])):
+			if (numbers[rowIndex][columnIndex] > largest):
+				largest = numbers[rowIndex][columnIndex]
+				numberIndex[0] = rowIndex
+				numberIndex[1] = columnIndex
+
+	return numberIndex
+
+def task_two(word):
+
+	outcome = ""
+	for index in range(len(word)):
+		count = 0
+		letter = word[index]
+		if letter not in outcome:
+			for val in range(len(word)):
+				if letter == word[val]:
+					count += 1
+			outcome += f"{letter}{count}"
+	return outcome
--- Python/Gate Three/day_three_task_test.py
+++ Python/Gate Three/day_three_task_test.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from day_three_task import task_one
+from day_three_task import *
 
 class TestClass (TestCase):
 
@@ -25,4 +25,25 @@ class TestClass (TestCase):
 		actual = task_one(number, 3)
 		expected = [10, 11, 12]
 
+		self.assertEqual(actual, expected)
+
+	def test_for_largest_number_in_2D_array(self):
+
+		number =[[45.2, 90.28, 77.1], [3.52, 9.86, 11.77], [7.7, 10, 22.33, 12]]
+		actual = task_three(number)
+		expected = [0,1]
+		self.assertEqual(actual, expected)
+
+	def test_the_number_of_characters(self):
+
+		words = "AaaBBc"
+		actual = task_two(words)
+		expected = "A1a2B2c1"
+		self.assertEqual(actual, expected)
+
+	def test_for_another_of_the_number_of_characters(self):
+
+		words = "EEEEnnnnnee"
+		actual = task_two(words)
+		expected = "E4n5e2"
 		self.assertEqual(actual, expected)
